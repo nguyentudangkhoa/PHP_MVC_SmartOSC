2f92d5d9146af35af492c24ed8122c321bfa6d81
khoantd <khoantd@smartosc.com>
2020-09-15 22:02:26 +0700

    change

--- public/assets/js/form/fileUpload.js
+++ public/assets/js/form/fileUpload.js
@@ -1,20 +1,9 @@
 $(document).ready(function() {
-    var specialChars = "<>@!#$%^&*()_+[]{}?;|'\"\\,./~`-="
-    var check = function(string) {
-        for (i = 0; i < specialChars.length; i++) {
-            if (string.indexOf(specialChars[i]) > -1) {
-                return true
-            }
-        }
-        return false;
-    }
     $('#form-game').submit(function(e) {
         e.preventDefault();
         var form_data = new FormData(this);
         if (form_data.get('name') == "") {
             alert("Name of the game cant empty");
-        } else if (check(form_data.get('name')) == true) {
-            alert("Name of game cant have special character");
         } else if (form_data.get('price') == "") {
             alert("Price cant empty");
         } else if (isNaN(parseInt(form_data.get('price')))) {
@@ -30,7 +19,7 @@ $(document).ready(function() {
         } else {
             $.ajax({
                 type: "POST",
-                url: './index.php?controller=admin&action=addProduct',
+                url: '',
                 data: {
                     name: form_data.get('name'),
                     price: form_data.get('price'),

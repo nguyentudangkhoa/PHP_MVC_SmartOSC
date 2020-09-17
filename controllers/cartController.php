895b0704a3b94fedf33f816cffc8619a9bfdc6c2
khoantd <khoantd@smartosc.com>
2020-09-17 10:19:49 +0700

    fix bug shopping cart

--- controllers/cartController.php
+++ controllers/cartController.php
@@ -21,39 +21,43 @@ class CartController extends BaseController
   }
   public function addToCart(){
     $id = $_POST['id'];
-    $product = $this->model('Game')->GetSingle($id)->fetch_array();
+    $product = $this->model('Game')->GetSingle($id)->fetch_array();// get product
   
-    if(!isset($_SESSION['cart'])){
+    if(!isset($_SESSION['cart'])){// check exists session cart
       $_SESSION['cart'] = array(
         "items"       => array(
-                                $id =>  array(
+                                $id =>  [
                                   "quantity"  =>  1,
                                   "price"     =>  $product['price'], 
                                   "item"     =>  $product
-                                )
+                                ]
                               ),
         "totalQty"    =>  1,
         "totalPrice"  =>$product['price']
       );
       echo json_encode(array('report' => 'Add product to cart success!', 'total'=>$_SESSION['cart']['totalQty']));
     }else{
-      if(array_key_exists($id,$_SESSION['cart']['items'])==true ){
-        $_SESSION['cart']['items'][$id]['quantity'] += 1;
-        $_SESSION['cart']['items'][$id]['price'] += $product['price'];
+      if(array_key_exists($id,$_SESSION['cart']['items'])==true ){// check key exists
+        $_SESSION['cart']['items'][$id]['quantity']+= 1;
+        $_SESSION['cart']['items'][$id]['price']+= $product['price'];
         $_SESSION['cart']['totalQty'] += 1;
         $_SESSION['cart']['totalPrice']+= $product['price'];
         echo json_encode(array('report' => 'Add product to cart success!', 'total'=>$_SESSION['cart']['totalQty']));
       }else {
         $_SESSION['cart']['totalQty'] += 1;
         $_SESSION['cart']['totalPrice']+= $product['price'];
-        $item = array(
-                  $id => array(
-                    'quantity'  =>  1,
-                    'price'     =>  $product['price'],
-                    'item'     =>  $product
-                  )
-                );
-        array_push($_SESSION['cart']['items'],$item);
+        array_push($_SESSION['cart']['items'],(int)$id);// push key to array
+        $_SESSION['cart']['items'][(int)$id] = [
+          'quantity'  =>  1,
+          'price'     =>  $product['price'],
+          'item'     =>  $product
+        ];// add the value for the key you added before
+        foreach($_SESSION['cart']['items'] as $key=>$value ){
+          if(!is_array($value)){
+            unset($_SESSION['cart']['items'][$key]);
+          }
+        }// check if the value is not an array delete it 
+        
         echo json_encode(array('report' => 'Add product to cart success!', 'total'=>$_SESSION['cart']['totalQty']));
       }
     }
--- views/pages/cart.php
+++ views/pages/cart.php
@@ -1,10 +1,31 @@
-
-<table>
-<?php 
-ini_set('display_errors', 1);//Show error of code
-ini_set('display_startup_errors', 1);//Show error of code
-error_reporting(E_ALL);//Show error of code
-
-    var_dump( $_SESSION['cart']);
-?>
+<style>
+    table{
+        margin-top: 40px;
+        margin-bottom: 40px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    table, th, td {
+         border: 1px solid grey;
+    }
+    h1{
+        text-align:center
+    }
+</style>
+<h1>Shopping Cart</h1>
+<table >
+<tr style="text-align: center; color:whitesmoke; background-color:black">
+<th style="padding: 10px;">Name</th>
+<th style="padding: 10px;">Quantity</th>
+<th style="padding: 10px;">Price</th>
+<th style="padding: 10px;">Total price of game</th>
+</tr>
+<?php foreach($_SESSION['cart']['items'] as $key=>$cart){ ?>
+<tr>
+<td style="padding: 10px;"><?php echo $cart['item']['name'] ?></td>
+<td style="padding: 10px;"><?php echo $cart['quantity'] ?></td>
+<td style="padding: 10px;"><?php echo $cart['item']['price'] ?></td>
+<td style="padding: 10px;"><?php echo $cart['price'] ?></td>
+</tr>
+<?php } ?>
 </table>
